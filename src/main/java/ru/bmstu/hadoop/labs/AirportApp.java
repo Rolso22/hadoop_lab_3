daab1f23bb47447dc1d7fea04f54cf9ce2aeab33
Rolso22 <ramagf@yandex.ru>
2021-12-03 20:52:35 +0300

    Scripted auto-commit on change (2021-12-03 20:52:35) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
@@ -1,7 +1,13 @@
 package ru.bmstu.hadoop.labs;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class AirportApp {
     public static void main(String[] args) {
-        
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+
     }
 }
