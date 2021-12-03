31d8809a27f4dc65e7757a5bea1a3e12a0caa020
Rolso22 <ramagf@yandex.ru>
2021-12-03 23:11:46 +0300

    Scripted auto-commit on change (2021-12-03 23:11:46) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
@@ -33,7 +33,8 @@ public class AirportApp {
                     return new Tuple2<>(new Tuple2<>(originPort, destPort), delay);
                 })
                 .combineByKey(new CreateCombiner(), new MergeValue(), new MergeCombiners());
-        final Broadcast<Map<String, String>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
+        final Broadcast<Map<String, String>> airportsBroadcasted = sc.broadcast(airportMap);
+        
     }
 
 }
