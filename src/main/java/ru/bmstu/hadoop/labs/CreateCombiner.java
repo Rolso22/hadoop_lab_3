cbb24682f17076a78ac7edc927243441c1291209
Rolso22 <ramagf@yandex.ru>
2021-12-03 23:03:13 +0300

    Scripted auto-commit on change (2021-12-03 23:03:13) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/CreateCombiner.java
+++ src/main/java/ru/bmstu/hadoop/labs/CreateCombiner.java
@@ -6,7 +6,7 @@ import org.apache.spark.api.java.function.Function;
 public class CreateCombiner implements Function<String, Flight> {
 
     @Override
-    public Flight call(String s) throws Exception {
+    public Flight call(String delay) throws Exception {
         return null;
     }
 }
