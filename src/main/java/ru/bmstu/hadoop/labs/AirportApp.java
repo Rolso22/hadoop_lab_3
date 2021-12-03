68ad3ddf91144f28d0a0d656deb01c0046c039d2
Rolso22 <ramagf@yandex.ru>
2021-12-03 22:49:19 +0300

    Scripted auto-commit on change (2021-12-03 22:49:19) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
@@ -31,6 +31,6 @@ public class AirportApp {
                     String delay = lineParts[DELAY_TIME_INDEX];
                     return new Tuple2<>(new Tuple2<>(originPort, destPort), delay);
                 })
-                .combineByKey();
+                .combineByKey(Combiner.createCombiner,  );
     }
 }
