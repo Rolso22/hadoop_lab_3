5d3df56f063babacf65848e2f38158cdbb6eaa43
Rolso22 <ramagf@yandex.ru>
2021-12-03 23:54:36 +0300

    Scripted auto-commit on change (2021-12-03 23:54:36) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/Flight.java
+++ src/main/java/ru/bmstu/hadoop/labs/Flight.java
@@ -18,11 +18,13 @@ public class Flight implements Serializable {
         this.flightCount = flightCount;
     }
 
-    public static Flight createFlight(String delay) {
+    public static Function createFlight(String delay) {
         boolean isCancelled = delay.isEmpty();
         float maxDelay = isCancelled ? 0 : Float.parseFloat(delay);
         boolean isDelayed = maxDelay > 0;
-        return new Flight(maxDelay, isCancelled, isDelayed ? 1 : 0, isCancelled ? 1 : 0, 1);
-        Function<String, Flight> create = 
+        //return new Flight(maxDelay, isCancelled, isDelayed ? 1 : 0, isCancelled ? 1 : 0, 1);
+        return new Function<String, Flight>() {
+            
+        }
     }
 }
