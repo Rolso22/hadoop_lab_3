937d703cfa376e64de49eb0ff40f7d4e15bedac4
Rolso22 <ramagf@yandex.ru>
2021-12-03 21:05:55 +0300

    Scripted auto-commit on change (2021-12-03 21:05:55) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class AirportApp {
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class AirportApp {
         JavaRDD<String> airportFile = sc.textFile("L_AIRPORT_ID.csv");
         JavaPairRDD<String, String> airportMap = airportFile.flatMapToPair(str -> {
                     String[] lineParts = str.split(",");
-                    
+                    return new Tuple2<>(lineParts[0], lineParts[1])
                 }
         )
     }
