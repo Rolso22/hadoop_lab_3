2f24b152b0a06798385f7cba54ff3a7c2a270e7f
Rolso22 <ramagf@yandex.ru>
2021-12-03 20:56:00 +0300

    Scripted auto-commit on change (2021-12-03 20:56:00) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/AirportApp.java
@@ -9,6 +9,6 @@ public class AirportApp {
         SparkConf conf = new SparkConf().setAppName("AirportApp_lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airportFile = sc.textFile("L_AIRPORT_ID.csv");
-
+        
     }
 }
