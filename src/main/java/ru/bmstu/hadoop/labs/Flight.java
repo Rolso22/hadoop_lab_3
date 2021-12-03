4ba49b8ab17eb9ad991dac79afc2862beebb240b
Rolso22 <ramagf@yandex.ru>
2021-12-03 23:45:19 +0300

    Scripted auto-commit on change (2021-12-03 23:45:19) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/Flight.java
+++ src/main/java/ru/bmstu/hadoop/labs/Flight.java
@@ -19,6 +19,7 @@ public class Flight implements Serializable {
 
     public static Flight createFlight(String delay) {
         boolean isCancelled = delay.isEmpty();
+        float maxDelay = isCancelled? 0 : Float.parseFloat(delay);
         
     }
 }
