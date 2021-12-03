8fbd2eea961f28eeba85e9d051bff35253cc1643
Rolso22 <ramagf@yandex.ru>
2021-12-03 21:14:03 +0300

    Scripted auto-commit on change (2021-12-03 21:14:03) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/Constants.java
+++ src/main/java/ru/bmstu/hadoop/labs/Constants.java
@@ -1,4 +1,12 @@
 package ru.bmstu.hadoop.labs;
 
 public class Constants {
+    public static final int DELAY_TIME_INDEX = 18;
+    public static final int AIRPORT_INDEX = 14;
+    public static final int AIRPORT_FLAG = 0;
+    public static final int FLIGHT_FLAG = 1;
+    public static final int CODE_INDEX = 0;
+    public static final int DESCRIPTION_INDEX = 1;
+    public static final String DELIMITER_COMMA_WITH_QUOTES = "\",\"";
+    public static final String DELIMITER_COMMA = ",";
 }
