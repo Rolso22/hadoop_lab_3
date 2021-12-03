f5a6b2996bb7f515770e2f33e06e78506ac5282f
Rolso22 <ramagf@yandex.ru>
2021-12-03 23:03:50 +0300

    Scripted auto-commit on change (2021-12-03 23:03:50) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/MergeValue.java
+++ src/main/java/ru/bmstu/hadoop/labs/MergeValue.java
@@ -1,8 +1,8 @@
 package ru.bmstu.hadoop.labs;
 
-import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
 
-public class MergeValue implements Function<Flight, String, Flight> {
+public class MergeValue implements Function2<Flight, String, Flight> {
 
     @Override
     public Flight call(Flight flight, String delay) throws Exception {
