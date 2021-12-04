543eb37b02742b11a5ee756694db8d3bb27f6004
Rolso22 <ramagf@yandex.ru>
2021-12-04 09:38:43 +0300

    Scripted auto-commit on change (2021-12-04 09:38:43) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/Constants.java
+++ src/main/java/ru/bmstu/hadoop/labs/Constants.java
@@ -9,5 +9,5 @@ public class Constants {
     public static final String DELIMITER_COMMA_WITH_QUOTES = "\",\"";
     public static final String DELIMITER_COMMA = ",";
     public static final String CODE = "Code";
-    public static final String YEAR = "\"ARR_DELAY_NEW\"";
+    public static final String YEAR = "\"YEAR\"";
 }
